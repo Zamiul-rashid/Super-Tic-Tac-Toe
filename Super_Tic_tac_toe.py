c79a3774f5bb9b69a87e51a9efa7198fc5e4f328
proyas21 <abyashrirproyas@gmail.com>
2023-11-16 02:43:46 +0600

    aesthetic color update, changes input numbering

--- Super_Tic_tac_toe.py
+++ Super_Tic_tac_toe.py
@@ -3,8 +3,15 @@ import os
 def cls():
     os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
+disabled = '\033[02m'
+highlight_color = '\033[47m'
+X_color = '\033[01m'+'\033[93m'
+Y_color = '\033[01m'+'\033[36m'
+RESET = "\033[0m"
+
+
 def logoDisplay():
-    print(
+    print('\033[35m',
         """
    ____                                             
   / __/_ _____  ___ ____                            
@@ -14,17 +21,17 @@ def logoDisplay():
 /_  __(_)___ ___ /_  __/__ _____ ___ /_  __/__  ___ 
  / / / / __//___/ / / / _ `/ __//___/ / / / _ \/ -_)
 /_/ /_/\__/      /_/  \_,_/\__/      /_/  \___/\__/ 
+
 """
-    )
+    ,end=RESET)
 
 
 class tic_tac_toe:
     def __init__(self):
         self.board = [
-            
-            ['0','1','2'],
-            ['3','4','5'],
-            ['6','7','8'],
+            ['1','2','3'],
+            ['4','5','6'],
+            ['7','8','9'],
         ]
             
         self.won = None
@@ -35,7 +42,7 @@ class tic_tac_toe:
                 print("","  | ".join(line))
                 print("---|---|---")
         else:
-            return(" │ ".join(self.board[lineno])) 
+            return " │ ".join(self.board[lineno])
     
     def checkWin(self):
         wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
@@ -46,7 +53,6 @@ class tic_tac_toe:
         
     def play (self,num,C_P):
         self.board[num//3][num%3]=C_P
-            
         self.won = self.checkWin() 
                      
                 
@@ -58,18 +64,37 @@ class super_tic_tac_toe:
             [tic_tac_toe(),tic_tac_toe(),tic_tac_toe()],
             [tic_tac_toe(),tic_tac_toe(),tic_tac_toe()],
         ]
-    def showboard (self,lineno = None ):    
+    def showboard (self, selected_board = None ):    
         print("╔"+"═"*(12*3-1)+"╗")
+
         for i in range(3):
             for k in range(3):  
+
                 print('║ ',end="")
+
                 for j in range(3):
-                    print(self.sboard[i][j].showboard(k),end=" ║ ")
+
+                    box_disabled = self.sboard[i][j].won!=None
+                    highlight = i*3+j == selected_board
+        
+                    line = ""
+                    for c in self.sboard[i][j].showboard(k):
+                        line += disabled if box_disabled else highlight_color if highlight else RESET
+                        if c=="X":
+                            line+=X_color+c+RESET # +last_color
+                        elif c=="O":
+                            line+=Y_color+c+RESET # +last_color
+                        else:
+                            line+=c
+                    print(line, end=RESET+" ║ ")
+                    # print(, end="")
+    
                 print()
                 if k == 2 :
-                    print("╬"+"═"*(12*3-1)+"╬")
+                    print("║"+"═"*(12*3-1)+"║")  # ╬
                 else:
                     print("║" + "---┼---┼---║"*3)
+                    
     def get_Board(self,boardno):
         return self.sboard[boardno//3][boardno%3]
     
@@ -84,22 +109,21 @@ def playgame():
     player = "X" 
     boardno = 4
     Tic = super_tic_tac_toe()
-    win_board = []
+
     while True:
         cls()
         logoDisplay()
-        Tic.showboard()
+        Tic.showboard(boardno)
         print()
         if Tic.checkWin() != None :
             """Print winner"""
             print(f"{Tic.checkWin()} is the winner !!!")
-            
             break
         while Tic.get_Board(boardno).won != None :
-            boardno = int(input(f"Please enter a board other then {boardno} : "))
+            boardno = int(input(f"Please enter a board other then {boardno+1} : "))-1
 
         
-        Inp = int(input(f"{player} Input : "))
+        Inp = int(input(f"{(X_color if player == 'X' else Y_color)+player} Input : {RESET}"))-1
      
         Tic.get_Board(boardno).play(Inp,player)
         
